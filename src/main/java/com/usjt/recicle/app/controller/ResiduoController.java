8dd5bb5f1c12661ba85517e84e82e4ecbeafc02a
Ingrid Lopes <ingridlopessudario27@icloud.com>
2024-05-21 14:49:48 -0300

    Update branch-ingrid

---
+++ src/main/java/com/usjt/recicle/app/controller/ResiduoController.java
@@ -0,0 +1,9 @@
+package com.usjt.recicle.app.controller;
+
+import com.usjt.recicle.app.model.CategoriaResiduo;
+import com.usjt.recicle.app.model.Residuo;
+
+public class ResiduoController {
+    
+
+}
